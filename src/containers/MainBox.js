012c09a31aa63c2916c07e67cdd98e9c034eb01a
allen-woods <allen.woods@flatironschool.com>
2019-04-06 16:18:24 -0700

    First commit. Offloaded conditionals into objects.

--- src/containers/MainBox.js
+++ src/containers/MainBox.js
@@ -4,21 +4,30 @@ import { Profile, Photos, Cocktails, Pokemon} from '../components/Pages.js'
 
 class MainBox extends React.Component {
 
+  state = {
+    active: 'profile'
+  }
+
+  handleActive = (active) => {
+    this.setState({active})
+  }
 
   render() {
 
-    /*
-
-    Replace the code below! Depending on what menu item is selected in the menu, I should render either a Profile, Photos, Cocktails, or Pokemon component.Think of a way to track which menu item was selected. Which component should have state? Which component should have methods to control state? Where should these methods be called?
-
-    */
-
-    const detailsToDisplay = <div>Hi, I'm a div!</div>
+    const details = {
+      'profile':  <Profile />,
+      'photo':    <Photos />,
+      'cocktail': <Cocktails />,
+      'pokemon':  <Pokemon />
+    }
 
     return (
       <div>
-        <MenuBar />
-        {detailsToDisplay}
+        <MenuBar handleActive={this.handleActive} active={this.state.active} />
+        {
+          (!details[this.state.active]) ? null :
+          details[this.state.active]
+        }
       </div>
     )
   }
